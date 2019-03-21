bfafce60c8a04442af32b3b1956b9b10722684bb
aliaksandrruskevich <47434157+aliaksandrruskevich@users.noreply.github.com>
2019-03-21 21:11:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig) { 
+  var arr = str.split ('') 
+  var config = [].concat.apply([], bracketsConfig); 
+  var result; 
+  if (str.length % 2 != 0) { 
+  result = false;} 
+  else if (str.length % 2 === 0) { 
+  for ( var a = 0; a < arr.length; a++) { 
+  for ( var b = 0; b <config.length; b++) { 
+  var cond = arr[a] === config[b]; 
+  if (cond) { 
+  var cond1 = arr[a] === config[b]; 
+  var cond2 = arr[a+1] === config[b+1]; 
+  var cond3 = b%2 === 0; 
+  if (cond1 && cond2 && cond3) { 
+  arr.splice(a, 2); 
+  a = 0; 
+  b = -1; 
+  } 
+  } 
+  } 
+  } 
+  if (arr.length === 0) { 
+  result = true; 
+  } 
+  else result = false; 
+  } 
+  return result; 
+  }
